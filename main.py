e337bffded075c3b936c9ab4dfb5f9e76898e9de
Roshan2534 <r.saundankar@yahoo.in>
2023-01-15 12:50:55 -0500

    Added Commit Summary,etc

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import subprocess
+import subprocess, os
 
 def check_git_status():
     directory = input("Please enter the local git directory path: ")
-    git_check = subprocess.run(['git','rev-parse','--is-inside-work-tree'], cwd = directory, capture_output=True)
 
-    if git_check.returncode != 0:
-        while git_check.returncode != 0:
-            print("You have entered a Invalid git repository directory! Please try again.")
-            directory = input("Please enter the local git directory path: ")
-            git_check = subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'],  cwd = directory, capture_output=True)
+    while not os.path.isdir(directory) or not os.path.exists(os.path.join(directory, ".git")):
+        print("Invalid git repository directory. Please try again.")
+        directory = input("Please enter the local git directory path: ")
 
     git_status = subprocess.run(['git','status','--porcelain'], cwd=directory, capture_output=True)
     git_branch = subprocess.run(['git','rev-parse','--abbrev-ref','HEAD'], cwd=directory,capture_output=True)
@@ -41,6 +38,18 @@ def check_git_status():
         print("There are differences between the local and remote repositoy")
     else:
         print("The local and remote repository are in sync")
+
+    git_remote_status = subprocess.run(['git','remote','-v'],cwd=directory, capture_output=True)
+    remote_status = git_remote_status.stdout.decode()
+    print(f"Remote repository status:")
+    print(remote_status)
+
+    git_week_commits = subprocess.run(['git','log','--pretty=format: "%h %s"', '--since="1 week ago"'], cwd=directory, capture_output=True)
+    week_commits = git_week_commits.stdout.decode()
+    if not week_commits:
+        week_commits = "0"
+    print(f"Commits made in the last week:")
+    print(week_commits)
 
 
 if __name__ == '__main__':
